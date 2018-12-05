177bfaaa5a7b355512ebc7b9d0a60de950d9f190
wongjmh36 <45339425+wongjmh36@users.noreply.github.com>
2018-12-05 22:33:22 +0800

    Update TestStack.java

--- TestStack.java
+++ TestStack.java
@@ -2,40 +2,29 @@
 import java.util.Stack;
 
 public class TestStack {
-	public static void main (String args[]) {
-		Stack s = new Stack();				
-	
 
-		System.out.println(s);
+    public static void main(String args[]) {
+        Stack s = new Stack();
 
-		//System.out.println("Jason is at " + s.search("Patrick"));
+        System.out.println(s);
 
-		s.push(new Character('C'));
-		System.out.println(s);
-		s.push(new Character('D'));
-		System.out.println(s);
-		s.push("Jason");
-		System.out.println(s);
-		s.push("Peter");
-		System.out.println(s);
-		s.push(new Integer(123));
-		System.out.println(s);
-		s.push("Alice");
-		System.out.println(s);
-		s.push(new Character('Y'));
-		System.out.println(s);
-		s.push(new Double(700));
-		System.out.println(s);
+        s.push(new Character('A'));
+        //System.out.println("Pop the letter 'A' :");
+        //s.pop();
+        System.out.println(s);
+        s.push(new Integer(123));
+        System.out.println(s);
+        s.push("Jason");
+        System.out.println(s);
+        s.push(new Character('Y'));
+        System.out.println(s);
+        s.push(new Integer(456));
+        System.out.println(s);
 
-		System.out.println("peek() returns: " + s.peek());
+        System.out.println("peek() returns: " + s.peek());        
+        System.out.println("Jason is at " + s.search("Jason"));        
+        
+        System.out.println();
+    }
 
-		//System.out.println("Patrick is at " + s.search("Patrick"));
-		//System.out.println("A is at " + s.search(new Character('A')));
-		//System.out.println("789.123 is at " + s.search(new Double(789.123)));
-		//System.out.println("Peter is at " + s.search("Peter"));
-
-		
-		System.out.println();
-	}
-
-} 
+}
