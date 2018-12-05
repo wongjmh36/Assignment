2da6c6c5cc1231c3578b9728ae7470517c9f60fb
wongjmh36 <45339425+wongjmh36@users.noreply.github.com>
2018-12-06 04:54:46 +0800

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,41 +5,49 @@ class Game {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int data = 1 + (int) (Math.random() * 99);
-        int answer = 5;
+        int data = 1 + (int) (Math.random() * 1999);
+        int answer = 555;
         int guess;
 
         GameBinarySearchTree tree = new GameBinarySearchTree();
-        for (int k = 1; k <= 10; k++) {
+        for (int k = 1; k <= 2000; k++) {
             data = k;
             tree.insert(data);
-        } 
-        tree.inorder();
-        System.out.println(" Please guess a number among 1-2000");
+        }
+        //tree.inorder();
+        System.out.println(" Please guess a number between 1-2000");
 
         while (true) {
             guess = keyboard.nextInt();
             if (guess == answer) {
                 System.out.println("you win!");
                 break;
-            } else if (guess < answer) {
-
+            } else if (guess < answer)  {
                 for (int k = 1; k <= guess; k++) {
                     tree.delete(k);
                 }
-                
-                System.out.print("Please guess a number between");
-                tree.inorder();
+
+                System.out.print("Please guess a number between ");
+                tree.findMini();
+                System.out.print(" to ");
+                tree.findMaxi();
                 System.out.println("");
-            } else if (guess > answer) {
-                for (int j = 10; j >= guess; j--) {
+                
+            } else if (guess >= answer) {
+                for (int j = 2000; j >= guess; j--) {
                     tree.delete(j);
                 }
-                System.out.print("Please guess a number among ");
-                tree.inorder();
+             System.out.print("Please guess a number between");
+                tree.findMini();
+                System.out.print(" to ");
+                tree.findMaxi();
+                System.out.println("");
 
+            }else {
+                System.out.println("you lose!");
+                      
             }
-
+           
         }
 
     }
